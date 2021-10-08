bd6ddf75417ca22fc562a35fc601d3ddb5187608
John P. Alfaro <johnpaul899@hotmail.com>
2021-10-07 21:47:30 -0600

    Se solucionaron errores de version de modelos

--- Backend/src/models/Aeropuerto.js
+++ Backend/src/models/Aeropuerto.js
@@ -1,30 +1,7 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const Aeropuerto = mongoose.model(
     "Aeropuerto", 
-    new mongoose.Schema({ 
-        avion_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Avion',
-          },
-          ruta_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Ruta',
-          },
-          hora_sal: {
-            type: String,
-          },
-          hora_lleg: {
-            type: String,
-          }
-    })
-);
-
-module.exports = Aeropuerto;
-=======
-const Aeropuertos = mongoose.model(
-    "Aeropuertos", 
     new mongoose.Schema({
         nombre:String,
         pais:String,
@@ -34,5 +11,5 @@ const Aeropuertos = mongoose.model(
     })
 );
 
-module.exports = Aeropuertos;
->>>>>>> fdb151b6683a48baf3d15a2e97d8f5f900af0848
+module.exports = Aeropuerto;
+
--- Backend/src/models/Ruta.js
+++ Backend/src/models/Ruta.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const Ruta = mongoose.model(
     "Ruta", 
     new mongoose.Schema({ 
+        Inicio: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Aeropuertos'
+        },
+        Destino:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Aeropuertos'
+        },
         horario_id: [{
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Horario',
@@ -30,25 +37,3 @@ const Ruta = mongoose.model(
 );
 
 module.exports = Ruta;
-=======
-const Rutas = mongoose.model(
-    "Rutas", 
-    new mongoose.Schema({ 
-        Inicio: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Aeropuertos'
-        },
-        Destino:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Aeropuertos'
-        },
-        Duracion:Number,
-        latitud_Inicio:Number,
-        longitud_Inicio: Number,
-        latitud_Final:Number,
-        longitud_Final:Number
-    })
-);
-
-module.exports = Rutas;
->>>>>>> fdb151b6683a48baf3d15a2e97d8f5f900af0848
