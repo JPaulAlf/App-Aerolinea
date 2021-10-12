c64ccc1a88b777cffe128f974b93f38a8f299979
Diego <Juandiegoa907@gmail.com>
2021-10-12 02:47:31 -0600

    Se añadió la función de previsualización al componente de crear avión

--- Frontend/src/app/admin/avion/crear-avion/crear-avion.component.ts
+++ Frontend/src/app/admin/avion/crear-avion/crear-avion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-crear-avion',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-avion.component.css']
 })
 export class CrearAvionComponent implements OnInit {
+public archivos:any=[];
+public previsualizacion:string='';
 
-  constructor() { }
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
+
+
+
+
+  obtenerImagen(event:any){
+
+const imagen = event.target.files[0];
+
+this.extraerBase64(imagen).then((imagen: any) =>{
+  this.previsualizacion = imagen.base;
+  console.log(imagen.base);
+});
+
+
+this.archivos.push(imagen);
+  }
+
+
+
+extraerBase64 = async($event:any) => new Promise((resolve, reject) => {
+
+try {
+  const unsafeImg = window.URL.createObjectURL($event);
+  const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+  const reader = new FileReader();
+  reader.readAsDataURL($event);
+  reader.onload = () => {
+    resolve({
+      
+      base: reader.result
+
+    });
+  };
+  reader.onerror = error => {
+    resolve({
+      base: null
+    });
+    
+
+
+
+  }
+
+} catch (error) {
+ 
+}
+
+
+
+
+
+})
+
 
 }
