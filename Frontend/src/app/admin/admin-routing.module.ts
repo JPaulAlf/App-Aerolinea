2f0e8cf30bbaff26113c20ac87ce401c64ebaaf3
John P. Alfaro <johnpaul899@hotmail.com>
2021-10-11 23:28:19 -0600

    Se crearon las paginas para CRUD de Avion, en ADMIN

--- Frontend/src/app/admin/admin-routing.module.ts
+++ Frontend/src/app/admin/admin-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { InicioComponent } from '../admin/inicio/inicio.component';
+
+import { CrearAvionComponent } from '../admin/avion/crear-avion/crear-avion.component';
+import { EditarAvionComponent } from '../admin/avion/editar-avion/editar-avion.component';
+import { VerAvionComponent } from '../admin/avion/ver-avion/ver-avion.component';
 
 const routes: Routes = [
   {
     path: '', component: InicioComponent,
-    children:[
-      {path: 'adminDashboard', component: InicioComponent },
+    children: [
+      { path: 'adminDashboard', component: InicioComponent },
     ]
   },
-  //{path: 'admin', component: InicioComponent },
- 
-
+  { path: 'avion/crear-avion', component: CrearAvionComponent },
+  { path: 'avion/editar-avion', component: EditarAvionComponent },
+  { path: 'avion/ver-avion', component: VerAvionComponent },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
