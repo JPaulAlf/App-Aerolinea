9e9d2299aefe60aa2c7f6ad4d5d5ec48672f90ea
John P. Alfaro <johnpaul899@hotmail.com>
2021-10-13 11:55:25 -0600

    Se implemento garfico con chart.js 2.9.4 en admin

--- Frontend/src/app/admin/inicio/inicio.component.ts
+++ Frontend/src/app/admin/inicio/inicio.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-declare function ejecutarAnimacion():any;
-declare function menuMovil():any;
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+
+declare function ejecutarAnimacion(): any;
+declare function menuMovil(): any;
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -13,5 +15,34 @@ export class InicioComponent implements OnInit {
   ngOnInit(): void {
     ejecutarAnimacion();
   }
-  
+  canvas: any;
+  ctx: any;
+  @ViewChild('mychart') mychart: any;
+
+  ngAfterViewInit() {
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+
+    new Chart(this.ctx, {
+      type: 'line',
+        data: {
+          labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+          datasets: [{
+            label: "Last year's income",
+            data: [150000, 100000, 300000, 475000, 90000, 689000, 720000, 150000, 100000, 300000, 475000, 90000, 689000, 720000],
+            backgroundColor: [
+              'rgba(105, 0, 132, .2)',
+            ],
+            borderColor: [
+              'rgba(200, 99, 132, .7)',
+            ],
+            borderWidth: 2
+          }
+          ]
+        },
+        options: {
+          responsive: true
+        }
+      });
+  }
 }
