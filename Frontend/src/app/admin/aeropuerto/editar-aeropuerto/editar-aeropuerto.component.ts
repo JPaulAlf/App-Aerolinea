bac3e251cdd6313bf77e574b56f57c0d0012182c
Diego <Juandiegoa907@gmail.com>
2021-10-15 23:20:47 -0600

    Se añadió crud de aeropuerto faltando el mapa para seleccionar la ubicación

--- Frontend/src/app/admin/aeropuerto/editar-aeropuerto/editar-aeropuerto.component.ts
+++ Frontend/src/app/admin/aeropuerto/editar-aeropuerto/editar-aeropuerto.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-aeropuerto.component.css']
 })
 export class EditarAeropuertoComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
