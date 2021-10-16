ccf45c112454223502fec8ee41a8429f6bd830da
John P. Alfaro <johnpaul899@hotmail.com>
2021-10-15 22:14:18 -0600

    CRUD clientes listo, faltan campos en ingles

--- Frontend/src/app/admin/cliente/crear-cliente/crear-cliente.component.ts
+++ Frontend/src/app/admin/cliente/crear-cliente/crear-cliente.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-crear-cliente',
   templateUrl: './crear-cliente.component.html',
   styleUrls: ['./crear-cliente.component.css']
 })
-export class CrearClienteComponent implements OnInit {
 
-  constructor() { }
+export class CrearClienteComponent implements OnInit {
+ 
+  public archivos: any = [];
+  public previsualizacion: string = '';
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
+  
+  obtenerImagen(event: any) {
+    const imagen = event.target.files[0];
+    this.extraerBase64(imagen).then((imagen: any) => {
+      this.previsualizacion = imagen.base;
+      console.log(imagen.base);
+    }); 
+    this.archivos.push(imagen);
+  }
 
+  extraerBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      }
+    } catch (error) { }
+  })
 }
