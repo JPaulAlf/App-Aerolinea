610f73ebe5c3f175a04228c9e0ea64945d8cb207
Diego <Juandiegoa907@gmail.com>
2021-10-18 20:10:39 -0600

    Se implemento el mapa de aeropuerto

--- Frontend/src/app/admin/aeropuerto/crear-aeropuerto/crear-aeropuerto.component.ts
+++ Frontend/src/app/admin/aeropuerto/crear-aeropuerto/crear-aeropuerto.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Loader } from '@googlemaps/js-api-loader';
+import { styles } from './mapstyles';
 @Component({
   selector: 'app-crear-aeropuerto',
   templateUrl: './crear-aeropuerto.component.html',
   styleUrls: ['./crear-aeropuerto.component.css']
 })
 export class CrearAeropuertoComponent implements OnInit {
-
-  constructor() { }
+  private map!: google.maps.Map
+  
 
   ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyBrSzQLopheNl98oKL3xPgWCdQMK03ZPgA'
+    })
+
+    loader.load().then(() => {
+      console.log('loaded gmaps')
+
+      const location = { lat: 51.233334, lng: 	6.783333 }
+
+      this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: location,
+        zoom: 6,
+        styles: styles
+      })
+
+      const marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+      });
+    })
   }
 
 }
