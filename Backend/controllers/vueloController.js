26de0257220ca3ce7ddc22760fb59c002b8aab25
John P. Alfaro <johnpaul899@hotmail.com>
2021-10-27 11:59:35 -0600

    vuelo/Create modificacion para asientos de avion

--- Backend/controllers/vueloController.js
+++ Backend/controllers/vueloController.js
@@ -1,19 +1,38 @@
 const VueloModel = require("../models/Vuelo");
+const AvionModel = require("../models/Avion");
 
 module.exports.get = async (req, res, next) => {
-  const vuelos = await VueloModel.find().populate("Vuelo", "avion_id ruta_id hora_sal hora_lleg").exec();
+  const vuelos = await VueloModel.find().populate("avion_id ruta_id").exec();
   res.json(vuelos);
 };
 
 module.exports.getById = async (req, res, next) => {
   const id = req.params.id;
-  const vuelo = await VueloModel.findOne({ _id: id }).populate("Vuelo", "avion_id ruta_id hora_sal hora_lleg").exec();
+  const vuelo = await VueloModel.findOne({ _id: id }).populate("avion_id ruta_id").exec();
   res.json(vuelo);
 };
 
-module.exports.create = (req, res, next) => {
+module.exports.create = async (req, res, next) => {
   const { avion_id, ruta_id, hora_sal, hora_lleg } = req.body;
-  const vuelo = new VueloModel({ avion_id: avion_id, ruta_id: ruta_id, hora_sal: hora_sal, hora_lleg: hora_lleg });
+
+  const avion = await AvionModel.findById(avion_id).exec();
+  const cantFilas = avion.cant_filas;
+  const cantAsFil = avion.cant_af;
+  let arrAsientos_Con_Filas = [];
+  for (let i = 1; i <= cantFilas; i++) {
+    let fila = [];
+    for (let j = 1; j <= cantAsFil; j++) {
+      let asiento = {
+        "fil": i,
+        "num": j,
+        "est": 0
+      }
+      fila.push(asiento);
+    }
+    arrAsientos_Con_Filas.push(fila);
+  }
+
+  const vuelo = new VueloModel({ avion_id: avion_id, ruta_id: ruta_id, hora_sal: hora_sal, hora_lleg: hora_lleg, asientos: arrAsientos_Con_Filas });
   vuelo.save();
   res.json(vuelo);
 };
