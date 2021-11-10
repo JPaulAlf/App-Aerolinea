da1f2cd500692d9bb3fd0ac6cfc2ae742bf955c7
Diego <Juandiegoa907@gmail.com>
2021-11-09 22:15:29 -0600

    se creó la route de reserva y se añadió el app.use en index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,6 +15,7 @@ app.use('/api/avion', require('./routes/avion'));
 app.use('/api/ruta', require('./routes/ruta'));
 app.use('/api/usuario', require('./routes/usuario'));
 app.use('/api/vuelo', require('./routes/vuelo'));
+app.use('/api/reserva', require('./routes/reserva'));
 
 app.listen(process.env.PORT || 4000,() => {
 
---
+++ Backend/routes/reserva.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const { permit } = require("../middleware/authorization");
+const reservaController = require("../controllers/reservaController");
+
+
+
+router.get("/get", reservaController.get);
+
+router.get("/get/:id", reservaController.getById);
+
+router.post("/create/", reservaController.create);
+
+router.delete("/delete/:id", reservaController.delete);
+
+router.put("/update/:id", reservaController.update);
+
+
+module.exports = router;
