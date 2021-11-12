a7490f4a5f6dd48d8120033f45f36a5ef51c894f
John P. Alfaro <johnpaul899@hotmail.com>
2021-11-12 14:59:10 -0600

    Se elimino por el momento la seguridad de VUELO, para hacer pruebas con este modulo

--- Backend/routes/vuelo.js
+++ Backend/routes/vuelo.js
@@ -9,17 +9,18 @@ const { permit } = require("../middleware/authorization");
 //Definición de rutas para cada uno de los verbos para los post
 router.get("/get-flight", vueloController.get);
 
-router.get("/getBy-flight/:id", auth, vueloController.getById);
+router.get("/getBy-flight/:id", vueloController.getById);
 
 router.post("/add-flight",  vueloController.create);
 
-router.delete("/delete-flight/:id",auth, permit(1), vueloController.delete);
+router.delete("/delete-flight/:id", vueloController.delete);
 
-router.put("/update-flight-state/:id", vueloController.updateState);
+router.put("/update-flight-state/:id",vueloController.updateState);
 
 router.put("/update-flight-seat/:id", vueloController.updateSeat);
 
-router.put("/update-flight/:id",auth, permit(1), vueloController.update);
+router.put("/update-flight/:id", vueloController.update);
 
+//router.put("/update-flight/:id",auth, permit(1), vueloController.update);
 
 module.exports = router;
