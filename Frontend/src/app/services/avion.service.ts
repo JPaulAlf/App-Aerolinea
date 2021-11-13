e17e9aae6544735accf4d78b59e4143b691de767
Alvatrox4 <alvaro.rbarrantes@gmail.com>
2021-11-12 22:56:00 -0600

    Servicio de avion

---
+++ Frontend/src/app/services/avion.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+const BLOG_API_ENDPOINT = `${environment.apiUrl}/api/avion`;
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AvionService {
+  constructor(private http: HttpClient) {}
+
+  get(): Observable<any> {
+    return this.http.get(`${BLOG_API_ENDPOINT}/get`, httpOptions);
+  }
+
+  getById(id: string): Observable<any> {
+    return this.http.get(`${BLOG_API_ENDPOINT}/get/${id}`);
+  }
+
+  create(avion: any): Observable<any> {
+    return this.http.post(`${BLOG_API_ENDPOINT}/create/`, avion);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(`${BLOG_API_ENDPOINT}/delete/${id}`);
+  } 
+
+  update(id: string, avion: any): Observable<any> {
+    return this.http.put(`${BLOG_API_ENDPOINT}/update/${id}`,avion);
+  }
+}
