e6d605396cbc0506d2257c3260c70e8ffe9f0cd7
John P. Alfaro <johnpaul899@hotmail.com>
2021-11-16 12:22:33 -0600

    Error al mostrar mapa en Aeropuerto Edit, Resuelto

--- Frontend/src/app/admin/aeropuerto/crear-aeropuerto/crear-aeropuerto.component.ts
+++ Frontend/src/app/admin/aeropuerto/crear-aeropuerto/crear-aeropuerto.component.ts
@@ -12,29 +12,33 @@ import { AeropuertoService } from 'src/app/services/aeropuerto.service';
 @Component({
   selector: 'app-crear-aeropuerto',
   templateUrl: './crear-aeropuerto.component.html',
-  styleUrls: ['./crear-aeropuerto.component.css']
+  styleUrls: ['./crear-aeropuerto.component.css'],
 })
 export class CrearAeropuertoComponent implements OnInit {
-  private map!: google.maps.Map
-  constructor( private toastr:ToastrService, private aeropuertoS: AeropuertoService, private router: Router) { }
-
+  private map!: google.maps.Map;
+  constructor(
+    private toastr: ToastrService,
+    private aeropuertoS: AeropuertoService,
+    private router: Router
+  ) {}
 
   //formulario
-  aeropForm= new FormGroup({
-    nombre: new FormControl("", [Validators.required]),
+  aeropForm = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
     pais: new FormControl('', Validators.required),
     ciudad: new FormControl('', Validators.required),
     latitud: new FormControl('', Validators.required),
-    longitud: new FormControl('', Validators.required),descripcion: new FormControl('', Validators.required),
-    estado: new FormControl("", [Validators.required])
+    longitud: new FormControl('', Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    estado: new FormControl('', [Validators.required]),
   });
   reloadCurrentRoute() {
     const currentUrl = this.router.url;
-    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-        this.router.navigate([currentUrl]);
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
     });
   }
-  submitForm(){
+  submitForm() {
     if (this.aeropForm.valid) {
       this.aeropuertoS.create(this.aeropForm.value).subscribe((data) => {
         this.reloadCurrentRoute();
@@ -44,41 +48,45 @@ export class CrearAeropuertoComponent implements OnInit {
   ngOnInit(): void {
     this.aeropForm.get('estado')?.setValue(1);
     let loader = new Loader({
-      apiKey: 'AIzaSyBrSzQLopheNl98oKL3xPgWCdQMK03ZPgA'
-    })
+      apiKey: 'AIzaSyBrSzQLopheNl98oKL3xPgWCdQMK03ZPgA',
+    });
 
     loader.load().then(() => {
-
-      console.log('loaded gmaps')
-      var informacion = new google.maps.InfoWindow({
-
-      });
+      console.log('loaded gmaps');
+      var informacion = new google.maps.InfoWindow({});
 
       var location = { lat: 9.996182, lng: -84.210243 };
 
-      this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: location,
-        zoom: 6,
-        mapTypeControl: false,
-        styles: styles
-      })
+      this.map = new google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          center: location,
+          zoom: 6,
+          mapTypeControl: false,
+          styles: styles,
+        }
+      );
 
       const marker = new google.maps.Marker({
         map: this.map,
       });
 
-      this.map.addListener("click", (mapsMouseEvent: any) => {
-
+      this.map.addListener('click', (mapsMouseEvent: any) => {
         let lngLat = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
         location = JSON.parse(lngLat);
 
-        this.map.setCenter(location)
-        marker.setPosition(location)
+        this.map.setCenter(location);
+        marker.setPosition(location);
         this.aeropForm.get('latitud')?.setValue(location.lat.toString());
         this.aeropForm.get('longitud')?.setValue(location.lng.toString());
-        var textoMensaje = '<h4>You are here!</h4>' +
-          '<p><b>LNG: </b>' + location.lng + '</p>' +
-          '<p><b>LAT: </b>' + location.lat + '</p>';
+        var textoMensaje =
+          '<h4>You are here!</h4>' +
+          '<p><b>LNG: </b>' +
+          location.lng +
+          '</p>' +
+          '<p><b>LAT: </b>' +
+          location.lat +
+          '</p>';
 
         informacion.setContent(textoMensaje);
 
@@ -87,10 +95,7 @@ export class CrearAeropuertoComponent implements OnInit {
         marker.addListener('click', () => {
           informacion.open(this.map, marker);
         });
-
       });
-
     });
   }
-
 }
--- Frontend/src/app/admin/aeropuerto/editar-aeropuerto/editar-aeropuerto.component.ts
+++ Frontend/src/app/admin/aeropuerto/editar-aeropuerto/editar-aeropuerto.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 import { styles } from './mapstyles';
-import { Router , ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AeropuertoService } from 'src/app/services/aeropuerto.service';
@@ -13,117 +13,119 @@ import { waitForAsync } from '@angular/core/testing';
 @Component({
   selector: 'app-editar-aeropuerto',
   templateUrl: './editar-aeropuerto.component.html',
-  styleUrls: ['./editar-aeropuerto.component.css']
+  styleUrls: ['./editar-aeropuerto.component.css'],
 })
 export class EditarAeropuertoComponent implements OnInit {
-  
-  private map!: google.maps.Map
-  constructor( private toastr:ToastrService, private aeropuertoS: AeropuertoService, private router: Router, private aRouter: ActivatedRoute) {
-    this.id = this.aRouter.snapshot.paramMap.get('id')+'';
+  private map!: google.maps.Map;
+  constructor(
+    private toastr: ToastrService,
+    private aeropuertoS: AeropuertoService,
+    private router: Router,
+    private aRouter: ActivatedRoute
+  ) {
+    this.id = this.aRouter.snapshot.paramMap.get('id') + '';
     this.obtenerAeropuerto();
-   }
+  }
 
-
-  ae:any={};
-  lat:string=''; lon:string=''; id:string='';
+  ae: any = {};
+  lat: string = '';
+  lon: string = '';
+  id: string = '';
   //formulario
-  aeropForm= new FormGroup({
-    nombre: new FormControl("", [Validators.required]),
+  aeropForm = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
     pais: new FormControl('', Validators.required),
     ciudad: new FormControl('', Validators.required),
     latitud: new FormControl('', Validators.required),
-    longitud: new FormControl('', Validators.required),descripcion: new FormControl('', Validators.required),
-    estado: new FormControl("", [Validators.required]),
-    _id: new FormControl("",)
+    longitud: new FormControl('', Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    estado: new FormControl('', [Validators.required]),
+    _id: new FormControl(''),
   });
 
-  submitForm(){
+  submitForm() {
     if (this.aeropForm.valid) {
-      this.aeropuertoS.update(this.ae._id,this.aeropForm.value).subscribe((data) => {
-        this.router.navigate(['admin/airport/overview-airport']);
-      });
+      this.aeropuertoS
+        .update(this.ae._id, this.aeropForm.value)
+        .subscribe((data) => {
+          this.router.navigate(['admin/airport/overview-airport']);
+        });
     }
   }
   async obtenerAeropuerto() {
-    
-      this.aeropuertoS.getById(this.id).subscribe((data) => {
-        delete data.__v;
-        this.ae = data;
-        this.aeropForm.setValue(this.ae);
-        this.lat=data.latitud;
-        this.lon=data.longitud;
-        (async () => { 
-          // Do something before delay
-          console.log('before delay')
-  
-          await new Promise(f => setTimeout(f, 6000));
-
-          this.mapa();
-          // Do something after
-          console.log('after delay')
-      })();
-        
-      });
+    this.aeropuertoS.getById(this.id).subscribe((data) => {
+      delete data.__v;
+      this.ae = data;
+      this.aeropForm.setValue(this.ae);
+      this.lat = data.latitud;
+      this.lon = data.longitud;
+      this.mapa();
+    });
   }
-  ngOnInit(): void {
-  
-  }
-  mapa(){
+  ngOnInit(): void {}
+  mapa() {
     let loader = new Loader({
-      apiKey: 'AIzaSyBrSzQLopheNl98oKL3xPgWCdQMK03ZPgA'
-    })
-    
+      apiKey: 'AIzaSyBrSzQLopheNl98oKL3xPgWCdQMK03ZPgA',
+    });
+
     loader.load().then(() => {
-
-      
-      var informacion = new google.maps.InfoWindow({
-
-      });
-      console.log(this.lat+' '+this.lon)
+      var informacion = new google.maps.InfoWindow({});
+      console.log(this.lat + ' ' + this.lon);
       var location = { lat: +this.lat, lng: +this.lon };
-      console.log(location)
-      this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: location,
-        zoom: 6,
-        mapTypeControl: false,
-        styles: styles
-      })
+      console.log(location);
+      this.map = new google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          center: location,
+          zoom: 6,
+          mapTypeControl: false,
+          styles: styles,
+        }
+      );
 
       const marker = new google.maps.Marker({
         map: this.map,
       });
-      this.map.setCenter(location)
-        marker.setPosition(location)
-        this.lat = location.lat.toString();
-        this.lon = location.lng.toString();
-       
-        var textoMensaje = '<h4>You are here!</h4>' +
-          '<p><b>LNG: </b>' + location.lng + '</p>' +
-          '<p><b>LAT: </b>' + location.lat + '</p>';
+      this.map.setCenter(location);
+      marker.setPosition(location);
+      this.lat = location.lat.toString();
+      this.lon = location.lng.toString();
 
-        informacion.setContent(textoMensaje);
+      var textoMensaje =
+        '<h4>You are here!</h4>' +
+        '<p><b>LNG: </b>' +
+        location.lng +
+        '</p>' +
+        '<p><b>LAT: </b>' +
+        location.lat +
+        '</p>';
 
-        marker.setIcon('../../../../assets/images/pin.png');
+      informacion.setContent(textoMensaje);
 
-        marker.addListener('click', () => {
-          informacion.open(this.map, marker);
-         
-        });
+      marker.setIcon('../../../../assets/images/pin.png');
 
-      this.map.addListener("click", (mapsMouseEvent: any) => {
+      marker.addListener('click', () => {
+        informacion.open(this.map, marker);
+      });
 
+      this.map.addListener('click', (mapsMouseEvent: any) => {
         let lngLat = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
         location = JSON.parse(lngLat);
 
-        this.map.setCenter(location)
-        marker.setPosition(location)
-        
+        this.map.setCenter(location);
+        marker.setPosition(location);
+
         this.lat = location.lat.toString();
         this.lon = location.lng.toString();
-        
-        var textoMensaje = '<h4>You are here!</h4>' +
-          '<p><b>LNG: </b>' + location.lng + '</p>' +
-          '<p><b>LAT: </b>' + location.lat + '</p>';
+
+        var textoMensaje =
+          '<h4>You are here!</h4>' +
+          '<p><b>LNG: </b>' +
+          location.lng +
+          '</p>' +
+          '<p><b>LAT: </b>' +
+          location.lat +
+          '</p>';
 
         informacion.setContent(textoMensaje);
 
@@ -131,12 +133,8 @@ export class EditarAeropuertoComponent implements OnInit {
 
         marker.addListener('click', () => {
           informacion.open(this.map, marker);
-         
         });
-
       });
-
     });
   }
-
 }
