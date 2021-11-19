930e42716b7cb66371b7eb4623dc72b74bef0c21
John P. Alfaro <johnpaul899@hotmail.com>
2021-11-18 21:46:04 -0600

    Estructura de reserva lista, falta Filtro y Paypal

--- Frontend/src/app/client/reserva/reserva.component.ts
+++ Frontend/src/app/client/reserva/reserva.component.ts
@@ -6,7 +6,6 @@ import { AvionService } from 'src/app/services/avion.service';
 import { VueloService } from 'src/app/services/vuelo.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { RutaService } from 'src/app/services/ruta.service';
-import { forEach } from 'lodash';
 
 declare function ejecutarAnimacion(): any;
 declare function counterActivate(): any;
@@ -29,13 +28,25 @@ export class ReservaComponent implements OnInit {
   _aeropuertoInicio: any = [];
   _aeropuertoDestino: any = [];
 
-  _vuelosBusqueda_Con_Filtro: any = [];
+  //estos son los arrays que se tienen que llenar para el filtro en el
+  //Logica del frontEnd echa
+  _vuelosBusqueda_OneWay: any = [];
+  _vuelosBusqueda_RoundTrip1: any = [];
+  _vuelosBusqueda_RoundTrip2: any = [];
+
   p: any = 1;
 
-  selectFlight: string = 'False';
-  rutaSeleccionado: string = 'Route not selected!';
-  horarioSeleccionado: string = 'Schedule not selected!';
-  idVueloSeleccionado: string = 'Flight not selected!';
+  rutaSeleccionado_OneWay: string = 'Route not selected!';
+  horarioSeleccionado_OneWay: string = 'Schedule not selected!';
+  idVueloSeleccionado_OneWay: string = 'Flight not selected!';
+
+  rutaSeleccionado1_RoundTrip: string = 'Route not selected!';
+  horarioSeleccionado1_RoundTrip: string = 'Schedule not selected!';
+  idVueloSeleccionado1_RoundTrip: string = 'Flight not selected!';
+
+  rutaSeleccionado2_RoundTrip: string = 'Route not selected!';
+  horarioSeleccionado2_RoundTrip: string = 'Schedule not selected!';
+  idVueloSeleccionado2_RoundTrip: string = 'Flight not selected!';
 
   itemsForm = new FormGroup({
     inicio: new FormControl('', Validators.required),
@@ -44,8 +55,11 @@ export class ReservaComponent implements OnInit {
     fechaFinal: new FormControl('', Validators.required),
     selectRoundTrip: new FormControl(''),
     selectOneWay: new FormControl(''),
-    selectSearchFlight: new FormControl(''),
-    //selectFlight: new FormControl(''),
+    selectSearch_RoundTrip: new FormControl(''),
+    selectSearch_OneWay: new FormControl(''),
+    selectFlight_OneWay: new FormControl(''),
+    selectFlight1_RoundTrip: new FormControl(''),
+    selectFlight2_RoundTrip: new FormControl(''),
   });
 
   ngOnInit(): void {
@@ -56,77 +70,209 @@ export class ReservaComponent implements OnInit {
     });
   }
 
+  buscarVuelo() {
+    //Tienen el ID del aeropuerto seleccionado
+    var inicio_component = this.itemsForm.get('inicio')?.value;
+    var destino = this.itemsForm.get('destino')?.value;
+
+    //De tipo Date
+    var fechaInicio = this.itemsForm.get('fechaInicio')?.value;
+    var fechaFinal = this.itemsForm.get('fechaFinal')?.value;
+
+    //Tienen un valor TRUE o FALSE, segun fue la seleccion del usuario
+    var selectRoundTrip = this.itemsForm.get('selectRoundTrip')?.value;
+    var selectOneWay = this.itemsForm.get('selectOneWay')?.value;
+
+    //Si busca un RoundTrip,deben de realizarsen 2 tablas y 2 busquedas
+    if (this.itemsForm.get('selectRoundTrip')?.value == true) {
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(true);
+      this.itemsForm.get('selectSearch_OneWay')!.setValue(false);
+
+      //Metodo de busqueda y llenado de las 2 tablas
+      this.vueloService.get().subscribe((vuelo) => {
+        for (const item of vuelo) {
+          var date = new Date(item.horario_id.fecha);
+          //Se da formato a la fecha y se le concatena la hora de salida, para llamar solo 1 campo en el HTML
+          item.horario_id.fecha =
+            date.toLocaleDateString() + ' ' + item.horario_id.hora_sal;
+          item.ruta_id.descuento = item.ruta_id.descuento * 100;
+        }
+        var vuelosAuxiliares = [];
+        for (const item of vuelo) {
+          if (item.estado == 1) {
+            vuelosAuxiliares.push(item);
+          }
+        }
+        this._vuelosBusqueda_RoundTrip1 = vuelosAuxiliares.reverse();
+        this._vuelosBusqueda_RoundTrip2 = vuelosAuxiliares.reverse();
+      });
+    }
+
+    //Si busca un OneWay,debe de realizarse 1 tablas y 1 busqueda
+    if (
+      this.itemsForm.get('selectOneWay')!.value == true &&
+      this.itemsForm.get('selectRoundTrip')!.value == false
+    ) {
+      this.itemsForm.get('selectSearch_OneWay')?.setValue(true);
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(false);
+
+      //Metodo de busqueda y llenado de 1 tabla
+      this.vueloService.get().subscribe((vuelo) => {
+        for (const item of vuelo) {
+          var date = new Date(item.horario_id.fecha);
+          //Se da formato a la fecha y se le concatena la hora de salida, para llamar solo 1 campo en el HTML
+          item.horario_id.fecha =
+            date.toLocaleDateString() + ' ' + item.horario_id.hora_sal;
+          item.ruta_id.descuento = item.ruta_id.descuento * 100;
+        }
+        var vuelosAuxiliares = [];
+        for (const item of vuelo) {
+          if (item.estado == 1) {
+            vuelosAuxiliares.push(item);
+          }
+        }
+        this._vuelosBusqueda_OneWay = vuelosAuxiliares.reverse();
+      });
+    }
+
+    //En caso que de buscar y no tenga seleccionada ninguna opcion
+    if (
+      this.itemsForm.get('selectOneWay')!.value == false &&
+      this.itemsForm.get('selectRoundTrip')!.value == false
+    ) {
+      this.toastr.warning('Please select all options first', 'Flight');
+    }
+
+  }
+
+  //Esto debe de guardar la reserva en la Base de datos
+  formalizarReserva() {}
+
   seleccion_RoundTrip() {
     this.itemsForm.get('selectRoundTrip')?.setValue(true);
     this.itemsForm.get('selectOneWay')?.setValue(true);
+    this.itemsForm.get('selectSearch_OneWay')?.setValue(false);
+    this.itemsForm.get('selectFlight_OneWay')!.setValue(false);
   }
   seleccion_OneWay() {
-    this.itemsForm.get('selectRoundTrip')?.setValue(false);
     this.itemsForm.get('selectOneWay')?.setValue(true);
+    this.itemsForm.get('selectRoundTrip')?.setValue(false);
+    this.itemsForm.get('selectSearch_RoundTrip')!.setValue(false);
+    this.itemsForm.get('selectFlight1_RoundTrip')!.setValue(false);
+    this.itemsForm.get('selectFlight2_RoundTrip')!.setValue(false);
   }
 
-  buscarVuelo() {
-    this.itemsForm.get('selectSearchFlight')?.setValue(true);
-    //Busca el vuelo con el filtro de las variables seleccionadas
-    this.vuelosConFiltro();
-  }
-
-  SeleccionarVuelo(_id: any) {
-    //this.itemsForm.get('selectFlight')?.setValue(true);
-    this.selectFlight = 'true';
-    this.idVueloSeleccionado = _id;
-    this.vueloService.getById(this.idVueloSeleccionado).subscribe((data) => {
-      //Ruta seleccionada
-      this.rutaSeleccionado =
-        data.ruta_id.inicio.nombre + ' >>> ' + data.ruta_id.destino.nombre;
-
-      //Horario de la ruta seleccionada
-      var date = new Date(data.horario_id.fecha);
-      data.horario_id.fecha = date.toLocaleDateString();
-      this.horarioSeleccionado =
-        data.horario_id.fecha +
-        ' ' +
-        data.horario_id.hora_sal +
-        ' >>> ' +
-        data.hora_lleg;
-    });
+  SeleccionarVuelo_OneWay(_id: any) {
+    if (this.itemsForm.get('selectRoundTrip')?.value == true) {
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(true);
+      this.itemsForm.get('selectFlight1_RoundTrip')?.setValue(true);
+      this.itemsForm.get('selectFlight2_RoundTrip')?.setValue(true);
+      this.itemsForm.get('selectSearch_OneWay')!.setValue(false);
+    }
+    if (
+      this.itemsForm.get('selectOneWay')!.value == true &&
+      this.itemsForm.get('selectRoundTrip')!.value == false
+    ) {
+      this.itemsForm.get('selectSearch_OneWay')?.setValue(true);
+      this.itemsForm.get('selectFlight_OneWay')?.setValue(true);
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(false);
+      this.itemsForm.get('selectFlight1_RoundTrip')?.setValue(false);
+      this.itemsForm.get('selectFlight2_RoundTrip')?.setValue(false);
+    }
+    this.idVueloSeleccionado_OneWay = _id;
+    this.vueloService
+      .getById(this.idVueloSeleccionado_OneWay)
+      .subscribe((data) => {
+        //Ruta seleccionada
+        this.rutaSeleccionado_OneWay =
+          data.ruta_id.inicio.nombre + ' >>> ' + data.ruta_id.destino.nombre;
+        //Horario de la ruta seleccionada
+        var date = new Date(data.horario_id.fecha);
+        data.horario_id.fecha = date.toLocaleDateString();
+        this.horarioSeleccionado_OneWay =
+          data.horario_id.fecha +
+          ' ' +
+          data.horario_id.hora_sal +
+          ' >>> ' +
+          data.hora_lleg;
+      });
 
     // this.listarAsientos_Vuelo();
   }
+  SeleccionarVuelo1_RoundTrip(_id: any) {
+    if (this.itemsForm.get('selectRoundTrip')?.value == true) {
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(true);
+      this.itemsForm.get('selectFlight1_RoundTrip')?.setValue(true);
+      this.itemsForm.get('selectFlight2_RoundTrip')?.setValue(true);
+      this.itemsForm.get('selectSearch_OneWay')!.setValue(false);
+    }
+    if (
+      this.itemsForm.get('selectOneWay')!.value == true &&
+      this.itemsForm.get('selectRoundTrip')!.value == false
+    ) {
+      this.itemsForm.get('selectSearch_OneWay')?.setValue(true);
+      this.itemsForm.get('selectFlight_OneWay')?.setValue(true);
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(false);
+      this.itemsForm.get('selectFlight1_RoundTrip')?.setValue(false);
+      this.itemsForm.get('selectFlight2_RoundTrip')?.setValue(false);
+    }
+    this.idVueloSeleccionado1_RoundTrip = _id;
+    this.vueloService
+      .getById(this.idVueloSeleccionado1_RoundTrip)
+      .subscribe((data) => {
+        //Ruta seleccionada
+        this.rutaSeleccionado1_RoundTrip =
+          data.ruta_id.inicio.nombre + ' >>> ' + data.ruta_id.destino.nombre;
+        //Horario de la ruta seleccionada
+        var date = new Date(data.horario_id.fecha);
+        data.horario_id.fecha = date.toLocaleDateString();
+        this.horarioSeleccionado1_RoundTrip =
+          data.horario_id.fecha +
+          ' ' +
+          data.horario_id.hora_sal +
+          ' >>> ' +
+          data.hora_lleg;
+      });
 
-  vuelosConFiltro() {
-    // //Tienen el ID del aeropuerto seleccionado
-    // var inicio: any = this.itemsForm.get('inicio');
-    // var destino: any = this.itemsForm.get('destino');
-
-    // //De tipo Date
-    // var fechaInicio: any = this.itemsForm.get('fechaInicio');
-    // var fechaFinal: any = this.itemsForm.get('fechaFinal');
-
-    // //Tienen un valor TRUE o FALSE, segun fue la seleccion del usuario
-    // var selectRoundTrip: any = this.itemsForm.get('selectRoundTrip');
-    // var selectOneWay: any = this.itemsForm.get('selectOneWay');
-
-    //Metodo temporal, que lista TODOS los vuelos ACTIVOS
-    this.vueloService.get().subscribe((vuelo) => {
-      for (const item of vuelo) {
-        var date = new Date(item.horario_id.fecha);
-        //Se da formato a la fecha y se le concatena la hora de salida, para llamar solo 1 campo en el HTML
-        item.horario_id.fecha =
-          date.toLocaleDateString() + ' ' + item.horario_id.hora_sal;
-        item.ruta_id.descuento = item.ruta_id.descuento * 100;
-      }
-      var vuelosAuxiliares = [];
-      for (const item of vuelo) {
-        if (item.estado == 1) {
-          vuelosAuxiliares.push(item);
-        }
-      }
-      this._vuelosBusqueda_Con_Filtro = vuelosAuxiliares.reverse();
-    });
+    // this.listarAsientos_Vuelo();
   }
+  SeleccionarVuelo2_RoundTrip(_id: any) {
+    if (this.itemsForm.get('selectRoundTrip')?.value == true) {
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(true);
+      this.itemsForm.get('selectFlight1_RoundTrip')?.setValue(true);
+      this.itemsForm.get('selectFlight2_RoundTrip')?.setValue(true);
+      this.itemsForm.get('selectSearch_OneWay')!.setValue(false);
+    }
+    if (
+      this.itemsForm.get('selectOneWay')!.value == true &&
+      this.itemsForm.get('selectRoundTrip')!.value == false
+    ) {
+      this.itemsForm.get('selectSearch_OneWay')?.setValue(true);
+      this.itemsForm.get('selectFlight_OneWay')?.setValue(true);
+      this.itemsForm.get('selectSearch_RoundTrip')!.setValue(false);
+      this.itemsForm.get('selectFlight1_RoundTrip')?.setValue(false);
+      this.itemsForm.get('selectFlight2_RoundTrip')?.setValue(false);
+    }
+    this.idVueloSeleccionado2_RoundTrip = _id;
+    this.vueloService
+      .getById(this.idVueloSeleccionado2_RoundTrip)
+      .subscribe((data) => {
+        //Ruta seleccionada
+        this.rutaSeleccionado2_RoundTrip =
+          data.ruta_id.inicio.nombre + ' >>> ' + data.ruta_id.destino.nombre;
+        //Horario de la ruta seleccionada
+        var date = new Date(data.horario_id.fecha);
+        data.horario_id.fecha = date.toLocaleDateString();
+        this.horarioSeleccionado2_RoundTrip =
+          data.horario_id.fecha +
+          ' ' +
+          data.horario_id.hora_sal +
+          ' >>> ' +
+          data.hora_lleg;
+      });
 
-  formalizarReserva() {}
+    // this.listarAsientos_Vuelo();
+  }
 
   //
   //
