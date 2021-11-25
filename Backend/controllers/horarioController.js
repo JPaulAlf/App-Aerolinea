312692c52f3a7221042c9c93ab3b338ec6802b9a
Diego <Juandiegoa907@gmail.com>
2021-11-25 15:36:17 -0600

    Se corrigieron errores del mantenimiento de rutas

--- Backend/controllers/horarioController.js
+++ Backend/controllers/horarioController.js
@@ -7,65 +7,105 @@ module.exports.get = async (req, res, next) => {
 
 module.exports.getById = async (req, res, next) => {
   const id = req.params.id;
-  const horario = await HorarioModel.findOne({ _id: id }).exec();
+  const horario = await HorarioModel.findOne({
+    _id: id
+  }).exec();
   res.json(horario);
 };
 
 module.exports.create = (req, res, next) => {
-  const { fecha, hora_sal } = req.body;
-  const horario = new HorarioModel({ fecha: fecha, hora_sal: hora_sal });
+  const {
+    fecha,
+    hora_sal
+  } = req.body;
+  const horario = new HorarioModel({
+    fecha: fecha,
+    hora_sal: hora_sal
+  });
   horario.save();
   res.json(horario);
 };
 
 module.exports.delete = async (req, res, next) => {
+  console.log("entra delete")
+
+
+  var {
+    horarios
+  } = await RutaModel.findById(req.params.id).populate("horarios").exec();
+  var horariosAux = [];
+
+  for (const horario of req.body.horarios) {
+    console.log
+    if (!horario.inUse) {
+      if (horario._id != null) {
+        var horarioDB = await HorarioModel.findByIdAndRemove(horario._id);
+        console.log("NOTinUse")
+      }
+
+    }
+    if (horario.inUse) {
+      horariosAux.push(horario._id)
+      console.log("inUse")
+    }
 
 
 
-var {horarios} = await RutaModel.findById(req.params.id).populate("horarios").exec();
 
-for (const element of horarios) {
-  var horarioDB = await HorarioModel.findByIdAndRemove(element._id);
-}
-  
-    
+  }
 
-  
+
+
+
+  req.horarios_id = horariosAux;
+
+
+
+
 
   // si post es null significa que no existe el registro
- return next();
- 
+  return next();
+
 };
 
 module.exports.update = async (req, res, next) => {
 
-//borrar
+  //borrar
 
 
 
- 
-//crear
-  var horarios_id=[];
-  
+
+  //crear
+  var horarios_id = [];
+  console.log(req.horarios_id)
+
+  console.log(req.body.horarios)
+  for (const element of req.body.horarios) {
+
+    if (!element.inUse) {
+      const {
+        fecha,
+        hora_sal
+      } = element;
+     
+      const horario = await new HorarioModel({
+        fecha,
+        hora_sal
+      });
+      await horario.save();
+      req.horarios_id.push(horario._id);
+    }
 
 
-for (const element of req.body.horarios) {
 
-    const { fecha, hora_sal } = element;
-    const horario = await new HorarioModel({ 
-      fecha, hora_sal });
-   await horario.save();
-      horarios_id.push(horario._id);
-
-    
   }
-  
 
 
 
 
 
-req.horarios_id = horarios_id;
-  
+
+
+
   return next();
 };
